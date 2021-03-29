b6476e9fb0f06cab815f709af3a95d508efe5e9d
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2021-03-29 14:09:18 +0800

    Modify controller and service

---
+++ app/WebService/Controllers/DemoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebService.Services;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<DemoService> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 2).Select(index => new DemoService
+            {
+                Desc = "Hello World " + index,
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55)
+            })
+            .ToArray();
+        }
+    }
+}
--- app/WebService/Services/DemoService.cs
+++ app/WebService/Services/DemoService.cs
@@ -2,14 +2,13 @@ using System;
 
 namespace WebService.Services
 {
-    public class WeatherForecast
+    public class DemoService
     {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
+        public string Desc { get; set; }
     }
 }
